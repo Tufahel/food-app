c61f3a165bde60f81ba738dadbaf9b2c043fea65
Tufahel <tufahel96@gmail.com>
2022-06-27 21:28:38 +0600

    controller methods

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,2 +1,13 @@
 class RecipeFoodsController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def show; end
+
+  def destroy; end
+
+  def recipe_food_params; end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,13 @@
 class RecipesController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def show; end
+
+  def destroy; end
+
+  def recipe_params; end
 end
