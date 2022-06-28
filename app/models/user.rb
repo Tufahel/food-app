88c7b74f9c7e9138bc1d23c455612c149c8979bd
unknown <okorojiebube2@gmail.com>
2022-06-28 03:37:33 -0600

    Add cancancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+
+  attr_accessor :name , :email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
+
+  Roles = [ :admin , :default ]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+  end
 end
