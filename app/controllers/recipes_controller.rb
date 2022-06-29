f31d8ec0c85d928c1ca674e8118dc1548ccc3267
Tufahel <tufahel96@gmail.com>
2022-06-29 16:54:09 +0600

    add recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,42 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all
+  end
 
-  def new; end
+  def new
+    @recipe = Recipe.new
+  end
 
-  def create; end
+  def create
+    @recipe = Recipe.new(recipe_params)
 
-  def show; end
+    if @recipe.save
+      flash[:success] = 'Recipe added successfully'
+      redirect_to recipes_path
+    else
+      flash.now[:error] = 'Error: Recipe could not be added'
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  def destroy; end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
-  def recipe_params; end
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      flash[:success] = 'Recipe was successfully deleted.'
+      redirect_to recipes_path
+    else
+      flash[:error] = 'Something went wrong'
+      render :show
+    end
+  end
+
+  def recipe_params
+    my_recipe = params.require(:recipe).permit(:name, :description)
+    my_recipe[:user] = current_user
+    my_recipe
+  end
 end
