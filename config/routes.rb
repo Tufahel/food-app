71976d368fa8f8ee9fb8d8eb4d6a61042687e2c7
Tufahel <tufahel96@gmail.com>
2022-07-02 03:19:45 +0600

    public recipe set and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'general_shopping_list/index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
@@ -6,13 +7,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "recipes#index"
+  get 'public_recipes', to: 'recipes#public', as: 'public'
+  root "recipes#public"
   resources :foods
   resources :recipes do
     resources :recipe_foods
     end
   resources :public_recipes, only: [:create, :destory] 
   resources :general_shopping_list, only: [:create, :destory]  
-
 
 end
