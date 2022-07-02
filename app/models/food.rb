7152667b427a2233e403bebcff57df7484600fa7
Tufahel <tufahel96@gmail.com>
2022-07-02 22:46:01 +0600

    refactor tests and added model test

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,8 +5,6 @@ class Food < ApplicationRecord
 
   validates :name, presence: true
 
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   validates :measurement_unit, presence: true, uniqueness: { case_sensitive: false }
